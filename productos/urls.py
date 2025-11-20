f04a6342b745917e20ac6de0e5cb3b1055dcbe21
isuarezt01 <isuarezt@unemi.edu.ec>
2025-11-19 21:47:30 -0500

    Actualización del proyecto Agronomía

--- productos/urls.py
+++ productos/urls.py
@@ -1,8 +1,11 @@
 # productos/urls.py
 from django.urls import path
-from . import views # Importa las vistas de la app actual
+from .views import login_view, logout_view, pagina_principal, register_view
 
 urlpatterns = [
     # Cuando la URL esté vacía (''), usa la vista 'pagina_principal'
-    path('', views.pagina_principal, name='principal'),
+    path('', pagina_principal, name='principal'),
+    path("login/", login_view, name="login"),
+    path("logout/", logout_view, name="logout"),
+    path("register/", register_view, name="register"),
 ]
--- productos/views.py
+++ productos/views.py
@@ -1,6 +1,56 @@
 # productos/views.py
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.models import User
+from django.contrib import messages
+from django.db import IntegrityError
 
 def pagina_principal(request):
-    # Esta función buscará 'index.html' dentro de la carpeta 'templates'
-    return render(request, 'index.html')
+    return render(request, "index.html")
+
+def login_view(request):
+    if request.method == "POST":
+        usuario = request.POST.get("usuario")
+        clave = request.POST.get("clave")
+
+        user = authenticate(request, username=usuario, password=clave)
+
+        if user is not None:
+            login(request, user)
+            return redirect("principal")
+        else:
+            messages.error(request, "Usuario o contraseña incorrectos")
+
+    return render(request, "login.html")
+
+def logout_view(request):
+    logout(request)
+    return redirect("login")
+
+def register_view(request):
+    if request.method == "POST":
+        username = request.POST.get("usuario")
+        email = request.POST.get("email")
+        password1 = request.POST.get("clave1")
+        password2 = request.POST.get("clave2")
+
+        if password1 != password2:
+            messages.error(request, "Las contraseñas no coinciden.")
+            return render(request, "register.html")
+
+        if len(password1) < 4:
+            messages.error(request, "La contraseña debe tener al menos 4 caracteres.")
+            return render(request, "register.html")
+
+        try:
+            user = User.objects.create_user(username=username, email=email, password=password1)
+            user.save()
+
+            login(request, user)
+            return redirect("principal")
+
+        except IntegrityError:
+            messages.error(request, "El usuario ya existe. Elige otro nombre.")
+            return render(request, "register.html")
+
+    return render(request, "register.html")
